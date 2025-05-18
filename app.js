d0265b5410ed54ad789693061537445b24b9e1a3
Noreh AD <akinoreh@gmail.com>
2025-05-19 00:07:48 +0300

    (feat) offset hours via wheel

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ let linearClockCanvas = document.getElementById("linear-clock");
 let linearClockContext = linearClockCanvas.getContext("2d");
 
 let oneHourWidthPx = 50;
+let offsetHours = 0;
 
 linearClockCanvas.width = oneHourWidthPx * 24;
 linearClockCanvas.height = 200;
 
-drawLinearClock(linearClockContext);
+drawLinearClock(linearClockContext, offsetHours);
+
+linearClockCanvas.addEventListener("wheel", (event) => {
+    event.preventDefault();
+    let dir = event.deltaY > 0 ? -1 : 1;
+    offsetHours += dir;
+    drawLinearClock(linearClockContext, offsetHours);
+});
--- clock-utils.js
+++ clock-utils.js
@@ -1,4 +1,7 @@
 function drawLinearClock(context) {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    context.save();
+    context.translate(offsetHours * oneHourWidthPx, 0);
     for (let i = 0; i < 24; i++) {
         context.beginPath();
         context.moveTo(i * oneHourWidthPx + 0.5, 0);
@@ -12,4 +15,5 @@ function drawLinearClock(context) {
         context.fillStyle = "dimgray";
         context.fillText(i + 1, i * oneHourWidthPx + 5, 20);
     }
+    context.restore();
 }
