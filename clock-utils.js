ae2318b2ff41668b5c061bccc286355f1a6a477f
Noreh AD <akinoreh@gmail.com>
2025-05-19 16:37:29 +0300

    (feat) draw missing (offset) hours

--- clock-utils.js
+++ clock-utils.js
@@ -1,19 +1,27 @@
 function drawLinearClock(context) {
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
     context.save();
-    context.translate(offsetHours * oneHourWidthPx * -1, 0);
     for (let i = 0; i < 24; i++) {
+        let h = i + offsetHours;
+        h = loop(h, 0, 24, "close");
+
         context.beginPath();
         context.moveTo(i * oneHourWidthPx + 0.5, 0);
         context.lineTo(i * oneHourWidthPx + 0.5, context.canvas.height);
         context.strokeStyle = "silver";
+        if (h == 0) {
+            context.strokeStyle = "black";
+        }
         context.lineWidth = 1;
         context.stroke();
 
         context.font = "14px Arial";
         context.fontWeight = "bold";
         context.fillStyle = "dimgray";
-        context.fillText(i, i * oneHourWidthPx + 5, 20);
+        if (h == 0) {
+            context.fillStyle = "black";
+        }
+        context.fillText(h, i * oneHourWidthPx + 5, 20);
     }
     context.restore();
 }
