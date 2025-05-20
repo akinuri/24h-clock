61f341176c7638c715fba26dfddcafeaa9a9ee74
Noreh AD <akinoreh@gmail.com>
2025-05-20 22:53:14 +0300

    (refactor) app: dayWidthPx

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ let linearClockCanvas = document.getElementById("linear-clock");
 let linearClockContext = linearClockCanvas.getContext("2d");
 
 let oneHourWidthPx = 50;
+let dayWidthPx = oneHourWidthPx * 24;
 let offsetPx = 0;
 
-linearClockCanvas.width = oneHourWidthPx * 24;
+linearClockCanvas.width = dayWidthPx;
 linearClockCanvas.height = 200;
 
 drawLinearClock(linearClockContext);
